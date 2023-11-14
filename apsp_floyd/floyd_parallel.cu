b3b0ba8a5ff4662e71cd43bdd093706eee7f734c
GiovaneIwamoto <giovaneiwamoto@gmail.com>
2023-11-14 17:43:21 -0400

    fix: floyd parallel optmized

--- apsp_floyd/floyd_parallel.cu
+++ apsp_floyd/floyd_parallel.cu
@@ -8,7 +8,7 @@
 
 #define INFNTY INT_MAX
 
-int *graph_data;
+int **adjacency_matrix, **dp_matrix;
 int n_vertices;
 
 /* Undirected graph non-negative edge weights */
@@ -18,73 +18,55 @@ void generate_random_adj_matrix(int n_vertices)
     int i, j;
 
     /* Allocate memory for adjacency matrix 2D array */
-    graph_data = (int *)malloc(N * N * sizeof(int));
+    adjacency_matrix = (int **)malloc(N * sizeof(int *));
+
+    for (i = 0; i < N; i++)
+    {
+        adjacency_matrix[i] = (int *)malloc(N * sizeof(int));
+    }
+    srand(0);
+
     for (i = 0; i < N; i++)
     {
         for (j = i; j < N; j++)
         {
             if (i == j)
             {
-                graph_data[i * N + j] = 0; /* Diagonal */
+                adjacency_matrix[i][j] = 0;
             }
             else
             {
                 /* Zero to nine random */
                 int r = rand() % 10;
                 int val = (r == 2) ? INFNTY : r; /* No edge between vertices */
-                graph_data[i * N + j] = val;     /* Symmetrically */
-                graph_data[j * N + i] = val;     /* Symmetrically */
+                adjacency_matrix[i][j] = val;    /* Symmetrically */
+                adjacency_matrix[j][i] = val;    /* Symmetrically */
             }
         }
     }
 }
 
 /* Kernel CUDA Floyd algorithm */
-__global__ void floyd_warshall_parallel(int *dp, int N, int k)
+__global__ void floyd_warshall_parallel(int *dev_dp, int n_vertices, int k)
 {
-    int i = blockIdx.y * blockDim.y + threadIdx.y;
-    int j = blockIdx.x * blockDim.x + threadIdx.x;
+    int i, j;
+    int tid = (blockIdx.y * gridDim.x + blockIdx.x) * blockDim.x + threadIdx.x;
 
-    if (i < N && j < N) /* Within valid range of matrix */
+    if (tid < n_vertices * n_vertices) /* tid is within matrix dim */
     {
-        if (dp[i * N + k] != INFNTY && dp[k * N + j] != INFNTY) /* Path through vertex k exists */
+        i = tid / n_vertices;
+        j = tid - i * n_vertices;
+
+        if (dev_dp[tid] > (dev_dp[i * n_vertices + k]) + (dev_dp[k * n_vertices + j]))
         {
-            int new_dist = dp[i * N + k] + dp[k * N + j];
-            if (new_dist < dp[i * N + j])
-                dp[i * N + j] = new_dist;
+            dev_dp[tid] = (dev_dp[i * n_vertices + k]) + (dev_dp[k * n_vertices + j]);
         }
     }
 }
 
-void floyd_warshall_cuda(int *dp, int N)
-{
-    int *dev_dp;
-    size_t size = N * N * sizeof(int);
-
-    /* Allocate memory on GPU*/
-    cudaMalloc((void **)&dev_dp, size);
-
-    /* Copy data from CPU to GPU */
-    cudaMemcpy(dev_dp, dp, size, cudaMemcpyHostToDevice);
-
-    dim3 blockSize(16, 16);
-    dim3 gridSize((N + blockSize.x - 1) / blockSize.x, (N + blockSize.y - 1) / blockSize.y);
-
-    /* Execute kernel for each vertex, k pivot */
-    for (int k = 0; k < N; ++k)
-    {
-        floyd_warshall_parallel<<<gridSize, blockSize>>>(dev_dp, N, k);
-        cudaDeviceSynchronize(); /* Sync all kernels finished */
-    }
-    /* Copy data from GPU to CPU */
-    cudaMemcpy(dp, dev_dp, size, cudaMemcpyDeviceToHost);
-
-    /* Free GPU allocated memory */
-    cudaFree(dev_dp);
-}
-
 int main(int argc, char **argv)
 {
+    int i;
     if (argc != 2)
     {
         printf("USAGE: ./floyd_parallel <number_of_vertices>\n");
@@ -92,19 +74,47 @@ int main(int argc, char **argv)
     }
 
     n_vertices = atoi(argv[1]);
+
+    dp_matrix = (int **)malloc(n_vertices * sizeof(int *));
+    for (i = 0; i < n_vertices; i++)
+    {
+        dp_matrix[i] = (int *)malloc(n_vertices * sizeof(int));
+    }
+
     generate_random_adj_matrix(n_vertices);
 
-    int *dp_matrix = (int *)malloc(n_vertices * n_vertices * sizeof(int));
-    memcpy(dp_matrix, graph_data, n_vertices * n_vertices * sizeof(int));
+    dim3 blockSize(16, 16);
+    dim3 gridSize((n_vertices + blockSize.x - 1) / blockSize.x, (n_vertices + blockSize.y - 1) / blockSize.y);
+
+    int *dev_dp;
+    cudaMalloc((void **)&dev_dp, n_vertices * n_vertices * sizeof(int));
+
+    for (int i = 0; i < n_vertices; i++)
+    {
+        cudaMemcpy(dev_dp + i * n_vertices, adjacency_matrix[i], n_vertices * sizeof(int),
+                   cudaMemcpyHostToDevice);
+    }
 
     clock_t start = clock(); /* Start measuring execution time */
 
-    floyd_warshall_cuda(dp_matrix, n_vertices);
+    /* Execute kernel for each vertex, k pivot */
+    for (int k = 0; k < n_vertices; k++)
+    {
+        floyd_warshall_parallel<<<gridSize, blockSize>>>(dev_dp, n_vertices, k);
+        cudaDeviceSynchronize(); /* Sync all kernels finished */
+    }
 
     /* Stop measuring execution time */
     clock_t end = clock();
     float seconds = (float)(end - start) / CLOCKS_PER_SEC;
     printf("TOTAL ELAPSED TIME ON GPU = %f SECS\n", seconds);
+
+    /* Return dp matrix to CPU */
+    for (int i = 0; i < n_vertices; i++)
+    {
+        cudaMemcpy(dp_matrix[i], dev_dp + i * n_vertices, n_vertices * sizeof(int),
+                   cudaMemcpyDeviceToHost);
+    }
 
     /* Free allocated memory */
     free(dp_matrix);
